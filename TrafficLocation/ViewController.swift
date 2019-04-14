86d60b2314903e823f3e51b1a041a1aefeb8afa7
user149365 <user149365@92-63-141-196.rdns.melbourne.co.uk>
2019-04-14 15:25:58 +0100

    Initial Commit

---
+++ TrafficLocation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TrafficLocation
+//
+//  Created by user149365 on 4/14/19.
+//  Copyright © 2019 user149365. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
